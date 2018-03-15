e2d18e107fb89095dfd6f5efee73dab347137d13
Zachary Luebke <zach.verex@gmail.com>
2018-03-14 21:47:35 -0400

    Not much...

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 class Main {
   constructor() {
+    // Get Canvas DOM element.
+    this.canvas = $('#glcanvas')[0];
 
-    
+    // Get WebGL canvas context.
+    this.gl = this.canvas.getContext('webgl');
+
+    // Ensure WebGL is working.
+    if (!this.gl) {
+      console.error("WebGL is not supported by your browser.");
+      return;
+    }
+
+    // Start main loop.
+    requestAnimationFrame(() => this.tick());
   }
 
   tick() {
 
+    
+    // Request next tick.
+    requestAnimationFrame(() => this.tick());
   }
 
   update() {
