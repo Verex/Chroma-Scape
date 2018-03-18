7c8fccbd22a9e0e4a582a173fe8f412e28b412c3
jakethornton47 <jthornton3030@floridapoly.edu>
2018-03-18 14:19:02 -0400

    Program Wrapper Builder Design

--- js/renderer.js
+++ js/renderer.js
@@ -4,11 +4,14 @@
     A renderer object should be able to be extended for more specific use
 */
 class Renderer {
-  constructor() {
-    
-  }
+    constructor(glContext) {
+        this.program = new Program.Builder(glContext).
+               withShader("assets/shaders/vertex.glsl", glContext.VERTEX_SHADER, "VERTEX").
+               withShader("assets/shaders/fragment.glsl", glContext.FRAGMENT_SHADER, "FRAGMENT").
+               build();
+        
+    }
+    render() {
 
-  render() {
-
-  }
+    }
 }
