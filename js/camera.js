4b7f08627600388ec9d526c9d38734649539846f
Zach <zach.verex@gmail.com>
2018-03-21 14:26:56 -0400

    Added player class and fixed the perspective issues with resizing.

--- js/camera.js
+++ js/camera.js
@@ -2,26 +2,53 @@ class Camera extends Entity {
     constructor(width, height, eid, owner) {
         super(eid, owner, EntityType.ENTITY_CAMERA);
 
-        this.componentFactory.construct(ComponentID.COMPONENT_TRANSFORM);
-
+        // Assign width and height values.
         this.width = width;
         this.height = height;
 
-        this.aspectRatio = this.width / this.height;
-        this.projectionMatrix = mat4.create();
+        // Setup viewing perspective.
+        this.setupPerspective();
 
-        mat4.perspective(
-            this.projectionMatrix,
-            Math.radians(45),
-            this.aspectRatio,
-            0.1,
-            100.0
-        );
+        this.componentFactory.construct(ComponentID.COMPONENT_TRANSFORM);
 
         this.transformComponent = this.getComponent(ComponentID.COMPONENT_TRANSFORM);
-    }
-    tick(dt) {
+
         this.transformComponent.origin[Math.Z] = -10;
+    }
+
+    setupPerspective() {
+      // Calculate aspect ratio of canvas.
+      this.aspectRatio = this.width / this.height;
+
+      // Create projection matrix.
+      this.projectionMatrix = mat4.create();
+
+      // Apply perspective to projecction matrix.
+      mat4.perspective(
+          this.projectionMatrix,
+          Math.radians(45),
+          this.aspectRatio,
+          0.1,
+          100.0
+      );
+    }
+
+    tick(dt) {
+        //this.transformComponent.origin[Math.Z] = -10;
+        var globals = GlobalVars.getInstance();
+
+        // Check for canvas resize.
+        if (globals.clientWidth != this.width
+          || globals.clientHeight != this.height) {
+
+          // Update current width/height.
+          this.width = globals.clientWidth;
+          this.height = globals.clientHeight;
+
+          // Create new perspective.
+          this.setupPerspective();
+        }
+
         this.transformComponent.updateTransform();
 
         super.tick(dt);
@@ -37,4 +64,3 @@ EntityType.ENTITY_CAMERA.construction = (owner) => {
         owner
     );
 }
-
