6a2babe0ea1b3d53e5aff2695ba67674f57a94bb
jakethornton47 <jthornton3030@floridapoly.edu>
2018-08-11 11:06:40 -0400

    Small Fix

--- js/rendering/material.js
+++ js/rendering/material.js
@@ -6,7 +6,6 @@ class Material {
 
         for(var i = 0; i < this.shaders.length; i++) {
             this.shaders[i] = Assets.getInstance().getShader(this.shaders[i]);
-            if(this.shaders[i] )
         }
     }
 
