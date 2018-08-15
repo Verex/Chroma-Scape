8f86e44ffdbbb0c02674153d2b1ce9e54493991a
Jake <jthornton3030@floridapoly.edu>
2018-08-15 11:38:50 -0400

    More shader stuff

--- js/post-fx/postfx.js
+++ js/post-fx/postfx.js
@@ -1,3 +1,4 @@
+var blah = 0.0;
 class PostFX {
     constructor(ctx, target) {
         this.ctx = ctx;
@@ -25,6 +26,21 @@ class PostFX {
         this.renderTarget.bind();
         var positionLocation = this.program.attributeLocation("a_position");
         var texcoordLocation = this.program.attributeLocation("a_texCoord");
+        var screenSizeLocation = this.program.uniformLocation("u_screenSize");
+        var timeLocation = this.program.uniformLocation("time");
+        var distortionLocation = this.program.uniformLocation("distortion");
+        var distortion2Location = this.program.uniformLocation("distortion2");
+        var speedLocation = this.program.uniformLocation("speed");
+        var rollSpeedLocation = this.program.uniformLocation("rollSpeed");
+
+        this.ctx.uniform2f(screenSizeLocation, viewport.width, viewport.height);
+        var time = GlobalVars.getInstance().curtime;
+        this.ctx.uniform1f(timeLocation, time);
+        this.ctx.uniform1f(distortionLocation, 0.3);
+        this.ctx.uniform1f(distortion2Location, 0.3);
+        this.ctx.uniform1f(speedLocation, 0.3);
+        this.ctx.uniform1f(rollSpeedLocation, 0.0);
+
 
         this.ctx.enableVertexAttribArray(positionLocation);
 
